6485a436655862ab52f4a37cb1ed3feb36bed871
Purwoko Manual <purwokomanual@gmail.com>
2026-09-23 13:39:05 +0700

    Add files via upload

---
+++ js/tailwind-config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: { sans: ['"Plus Jakarta Sans"', 'sans-serif'] },
+        colors: {
+          darkBg: '#05070a', panelBg: '#0b0f19', panelBorder: '#1e293b',
+          sidebarBg: '#07090e', neonPurple: '#a855f7', neonCyan: '#06b6d4',
+          neonGreen: '#10b981', neonPink: '#ec4899', textMain: '#f8fafc', textMuted: '#64748b',
+        }
+      }
+    }
+  }
